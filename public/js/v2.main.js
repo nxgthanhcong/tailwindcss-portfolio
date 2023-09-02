3dc970624b0b92c6745c55b65346e3b02b0c8b31
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2023-09-02 17:43:10 +0700

    commit

--- public/js/v2.main.js
+++ public/js/v2.main.js
@@ -526,6 +526,23 @@ const UIController = {
       });
     });
   },
+
+  RegisterServiceWorker() {
+    //pwa
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log(
+            "Service Worker registered with scope:",
+            registration.scope,
+          );
+        })
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
+        });
+    }
+  },
 };
 
 (() => {
